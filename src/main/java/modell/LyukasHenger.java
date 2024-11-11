342ffbb1c82febbd4e8a22f08f280c2cf656dfbe
KAkos27 <akoskosztolanyi@gmail.com>
2024-11-11 09:20:11 +0100

    alap

---
+++ src/main/java/modell/LyukasHenger.java
@@ -0,0 +1,27 @@
+package modell;
+
+public class LyukasHenger extends TomorHenger {
+
+    private double falvastagsag;
+
+    public LyukasHenger(double sugar, double magassag, double falvastagsag) {
+        this(sugar, magassag, 1, falvastagsag);
+    }
+
+    public LyukasHenger(double sugar, double magassag, double fajsuly, double falvastagsag) {
+        super(sugar, magassag, fajsuly);
+        this.falvastagsag = falvastagsag;
+    }
+
+    @Override
+    public double terfogat() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "LyukasHenger{" +
+                "falvastagsag=" + falvastagsag +
+                '}';
+    }
+}
